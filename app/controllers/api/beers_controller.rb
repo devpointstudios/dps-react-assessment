d5516ea438ea9871c5d0a647fa26ee3f6ecada57
Jake Sorce <jakesorce@gmail.com>
2017-07-14 15:27:04 -0600

    Update beers_controller.rb

--- app/controllers/api/beers_controller.rb
+++ app/controllers/api/beers_controller.rb
@@ -4,14 +4,14 @@ class Api::BeersController < ApplicationController
   # brewery_db.beers.random
 
   def all
-    send_response(brew_client.beers.all)
+    send_response(brew_client.beers.all(p: @page))
   end
 
   def by_name
-    send_response(brew_client.beers.all(name: params[:name]))
+    send_response(brew_client.beers.all(name: params[:name], p: @page))
   end
 
   def random
-    send_response(brew_client.beers.random)
+    send_response(brew_client.beers.random(p: @page))
   end
 end
