720ecceb928018f546e88555c2b3f68a56aad23f
Jake Sorce <jakesorce@gmail.com>
2017-07-14 15:27:17 -0600

    Update breweries_controller.rb

--- app/controllers/api/breweries_controller.rb
+++ app/controllers/api/breweries_controller.rb
@@ -4,10 +4,10 @@ class Api::BreweriesController < ApplicationController
   # brewery_db.brewery('d1zSa7').beers
 
   def all
-    send_response(brew_client.breweries.all)
+    send_response(brew_client.breweries.all(p: @page))
   end
 
   def by_name
-    send_response(brew_client.breweries.all(name: params[:name]))
+    send_response(brew_client.breweries.all(name: params[:name], p: @page))
   end
 end
