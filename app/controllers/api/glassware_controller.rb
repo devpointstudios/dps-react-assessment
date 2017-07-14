bb75e1db094d11a12997dfbc4b6eaca5b6cece4d
Jake Sorce <jakesorce@gmail.com>
2017-07-14 15:27:59 -0600

    Update glassware_controller.rb

--- app/controllers/api/glassware_controller.rb
+++ app/controllers/api/glassware_controller.rb
@@ -1,8 +1,7 @@
 class Api::GlasswareController < ApplicationController
   # brewery_db.glassware.all
-  # brewery_db.glassware.find(1)
 
   def all
-    send_response(brew_client.glassware.all)
+    send_response(brew_client.glassware.all(p: @page))
   end
 end
