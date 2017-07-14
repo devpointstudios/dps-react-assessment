f8f183ba91e68c486ca2a31abe55534935a3efa5
Jake Sorce <jakesorce@gmail.com>
2017-07-14 15:27:29 -0600

    Update brewery_db_search_controller.rb

--- app/controllers/api/brewery_db_search_controller.rb
+++ app/controllers/api/brewery_db_search_controller.rb
@@ -4,14 +4,14 @@ class Api::BreweryDbSearchController < ApplicationController
   # brewery_db.search.breweries(q: 'IPA')
 
   def all
-    send_response(brew_client.search.all(q: params[:query]))
+    send_response(brew_client.search.all(q: params[:query], p: @page))
   end
 
   def beers
-    send_response(brew_client.search.beers(q: params[:query]))
+    send_response(brew_client.search.beers(q: params[:query], p: @page))
   end
 
   def breweries
-    send_response(brew_client.search.breweries(q: params[:query]))
+    send_response(brew_client.search.breweries(q: params[:query], p: @page))
   end
 end
