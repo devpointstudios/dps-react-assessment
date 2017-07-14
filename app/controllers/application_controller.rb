ccbb492b55aced86fe3de9cf31470c0182fb944c
Jake Sorce <jakesorce@gmail.com>
2017-07-14 09:56:24 -0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::API
   def send_response(api_response)
     if api_response.is_a?(BreweryDB::PaginatedCollection) || api_response.is_a?(BreweryDB::Collection)
       if(api_response.count > 0)
-        page = params[:page].to_i || 1
-        per_page = params[:per_page].to_i || BreweryDB::PaginatedCollection::BATCH_SIZE
+        page = params[:page] ? params[:page].to_i : 1
+        per_page = params[:per_page] ? params[:per_page].to_i : BreweryDB::PaginatedCollection::BATCH_SIZE
         paged_response = WillPaginate::Collection.create(page, per_page, api_response.count) do |pager|
           pager.replace api_response.take(per_page)
         end
